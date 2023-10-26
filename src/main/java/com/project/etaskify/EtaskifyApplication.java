644e82ac5222f6a5fca4f2212e9440574942ea9a
Vusale <vusalahasanova423@gmail.com>
2023-10-26 21:27:04 +0400

    a taskify project

---
+++ src/main/java/com/project/etaskify/EtaskifyApplication.java
@@ -0,0 +1,13 @@
+package com.project.etaskify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaskifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtaskifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/etaskify/EtaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.etaskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtaskifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
